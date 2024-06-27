7d16abf415e410c44bd234a50ae92045b5881080
Diego Sousa <pediegoss@gmail.com>
2024-06-26 22:13:34 -0300

    update

--- src/components/shoppingKart/ShoppingKark.tsx
+++ src/components/shoppingKart/ShoppingKark.tsx
@@ -1,12 +1,33 @@
 import { ReactElement } from "react";
-import { Container, NumberProducts } from "./Styles";
-import { RiShoppingCart2Fill } from "react-icons/ri";
+import { Container, NumberProducts, RiShoppingCart2FillIcons,
+  ProductsKart, TitleKart, PurchaseButton, DivProducts, DivPurchaseButton,
+  DivTotal, DivSubTotal, Total, SubTotal, TotalText, SubTotalText } from "./Styles";
+
 
 const ShoppingKart: React.FC = (): ReactElement => {
   return (
     <Container>
-      <RiShoppingCart2Fill />
+      <RiShoppingCart2FillIcons />
       <NumberProducts>0</NumberProducts>
+
+      <ProductsKart>
+        <TitleKart>RESUMO DO PEDIDO</TitleKart>
+
+        <DivPurchaseButton>
+          <PurchaseButton>Finalizar Pedido</PurchaseButton>
+        </DivPurchaseButton>
+
+        <DivProducts>
+          <DivSubTotal>
+            <SubTotalText>SUBTOTAL:</SubTotalText>
+            <SubTotal>R$ 150,00</SubTotal>
+          </DivSubTotal>
+          <DivTotal>
+            <TotalText>TOTAL:</TotalText>
+            <Total>R$ 150,00</Total>
+          </DivTotal>
+        </DivProducts>
+      </ProductsKart>
     </Container>
   )
 }
--- src/components/shoppingKart/Styles.ts
+++ src/components/shoppingKart/Styles.ts
@@ -1,9 +1,89 @@
 import styled from "styled-components";
+import { RiShoppingCart2Fill } from "react-icons/ri";
 
 export const Container = styled.div`
-  border: 1px solid red;
   padding: 5px;
+  position: relative;
 `;
 
 export const NumberProducts = styled.span`
+  font-size: 25px;
+`;
+
+export const RiShoppingCart2FillIcons = styled(RiShoppingCart2Fill)`
+  font-size: 35px;
+`;
+
+export const DivProducts = styled.div`
+  border: 1px solid grey;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const DivPurchaseButton = styled.div`
+  border-top: 1px solid grey;
+  padding: 10px 0;
+  display: flex;
+  justify-content: center;
+`;
+
+export const PurchaseButton = styled.button`
+  border: 0;
+  background-color: black;
+  color: white;
+  padding: 7px 18px;
+  display: inline-block;
+  font: 1rem bahnschriftRegular, sans-serif;
+  border-radius: 3px;
+  cursor: pointer;
+`;
+
+export const ProductsKart = styled.div`
+  position: absolute;
+  width: 280px;
+  border: 1px solid #3D194F;
+  z-index: 99;
+  top: 66px;
+  right: 4px;
+  background-color: #fff;
+  padding: 15px 0;
+`;
+
+export const TitleKart = styled.h4`
+  color: black;
+  font: 1.1rem bahnschriftRegular, sans-serif;
+  text-align: center;
+`;
+
+export const TotalText = styled.span`
+  font: 1rem bahnschriftRegular, sans-serif;
+`;
+
+export const Total = styled.span`
+  font: 1.1rem bahnschriftRegular, sans-serif;
+`;
+
+export const SubTotalText = styled.span`
+  font: 1.1rem bahnschriftRegular, sans-serif;
+`;
+
+export const SubTotal = styled.span`
+  font: 1rem bahnschriftRegular, sans-serif;
+`;
+
+export const DivSubTotal = styled.div`
+  font: 1.1rem bahnschriftRegular, sans-serif;
+  border-bottom: 1px solid grey;
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  width: 230px;
+`;
+
+export const DivTotal = styled.div`
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  width: 230px;
 `;
